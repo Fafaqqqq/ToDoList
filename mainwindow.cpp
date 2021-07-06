9abdf0765c145d3cc346ae8585809bbfaa51c12d
Fafaqqqq <loda_228@rambler.ru>
2021-07-06 17:59:43 +0300

    init

---
+++ mainwindow.cpp
@@ -0,0 +1,17 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+    , button(new QPushButton)
+{
+    ui->setupUi(this);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
